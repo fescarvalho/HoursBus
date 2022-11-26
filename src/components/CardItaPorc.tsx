6e54fa0f9d52377957f0d94ae7407f28545ea7df
fescarvalho <fernando_scarvalho@hotmail.com>
2022-11-26 16:07:12 -0300

    add animacao no title

--- src/components/CardItaPorc.tsx
+++ src/components/CardItaPorc.tsx
@@ -12,8 +12,11 @@ const CardItaPorc = ({ titleTo, titleFrom }: CardProps) => {
   };
 
   return (
-    <div className={styles.cardMain}>
-      <div className={styles.cardTitle} onClick={showClass}>
+    <div className={`${show === true ? styles.cardMainActive : styles.cardMain}`}>
+      <div
+        className={`${show === true ? styles.cardTitleActive : styles.cardTitle}`}
+        onClick={showClass}
+      >
         <h2>{titleFrom}</h2>
         <FaArrowDown />
         <h2>{titleTo}</h2>
--- src/components/CardPorcIta.tsx
+++ src/components/CardPorcIta.tsx
@@ -11,8 +11,11 @@ const CardPorcIta = ({ titleTo, titleFrom }: CardProps) => {
     setShow(!show);
   };
   return (
-    <div className={styles.cardMain}>
-      <div className={styles.cardTitle} onClick={showClass}>
+    <div className={`${show === true ? styles.cardMainActive : styles.cardMain}`}>
+      <div
+        className={`${show === true ? styles.cardTitleActive : styles.cardTitle}`}
+        onClick={showClass}
+      >
         <h2>{titleFrom}</h2>
         <FaArrowDown />
         <h2>{titleTo}</h2>
