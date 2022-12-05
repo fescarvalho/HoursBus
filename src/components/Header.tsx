49a225b74fa8fa689a8fde58c3dbab5e96fcb991
fescarvalho <fernando_scarvalho@hotmail.com>
2022-12-04 21:16:29 -0300

    ADD VARIAVEIS GLOBAIS NO CSS

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,16 +2,16 @@ import styles from "./Header.module.css";
 import { useState } from "react";
 
 const Header = () => {
-  const [dark, setDark] = useState(false);
+  /*   const [dark, setDark] = useState(false);
 
   function toggleTheme() {
     setDark(!dark);
     console.log(dark);
   }
-
+ */
   return (
     <div className={styles.header}>
-      <button onClick={() => toggleTheme()}>x</button>
+      {/*       <button onClick={() => toggleTheme()}>x</button> */}
       <h1>Horários de ônibus </h1>
     </div>
   );
